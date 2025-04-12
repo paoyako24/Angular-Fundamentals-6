5c46f7faceffff8f51aa17076ba8f91f4f3940bb
paoyako24 <paolaparedes248@gmail.com>
2025-04-12 17:57:54 -0600

    Add files via upload

---
+++ src/app/components/hero-item/hero-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, computed, input, output } from '@angular/core';
+import { Hero, PowerStat } from '../../shared/interfaces/hero.interface';
+
+import { CommonModule } from '@angular/common';
+import { HeroPowerstatsChange } from '../../shared/interfaces/hero-powerstats-change';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-hero-item',
+  /* TODO 606: Navigate to the view page (/hero/:id) and update page (/hero/update/:id) for each hero when clicking the navigation buttons. */
+  imports: [CommonModule, RouterLink],
+  templateUrl: './hero-item.component.html',
+})
+export class HeroItemComponent {
+  hero = input.required<Hero>();
+  powerstatsChange = output<HeroPowerstatsChange>();
+  isHeroVillain = computed(() => this.hero().alignment === "bad");
+
+  decrementPowerStats(powerstat: PowerStat): void{
+    this.powerstatsChange.emit({ hero: this.hero(), powerstat, value: -1 });
+  }
+
+  incrementPowerStats(powerstat: PowerStat): void{
+      this.powerstatsChange.emit({ hero: this.hero(), powerstat, value: 1 });
+  }
+}
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  /* TODO 604: Import RouterLink */
+  imports: [RouterLink],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+
+}
